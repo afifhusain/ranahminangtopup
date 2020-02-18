7bdc06f82f165a1982b577cf3e0d29d4647442c6
afifhusain <afifhusain2014@gmail.com>
2020-02-18 14:45:56 +0700

    commit kedua pasca

---
+++ application/views/template/footer.php
@@ -0,0 +1,15 @@
+<div class="footer-copyright">
+      <div class="container">
+      Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="<?= base_url();?>/desain/jquery-2.1.1.min.js"></script>
+  <script src="<?= base_url();?>/desain/js/materialize.js"></script>
+  <script src="<?= base_url();?>/desain/js/init.js"></script>
+
+  </body>
+</html>
